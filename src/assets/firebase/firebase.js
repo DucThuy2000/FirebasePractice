1826d7e9ed719b87634cf745a5330220e6b1e33b
Darwin <ducthuy1705@gmail.com>
2024-02-21 16:39:09 +0700

    feat: init and connect to firebase project

---
+++ src/assets/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAaOts65QFprRThf1RyCfQg-DweNtF4scg',
+  authDomain: 'fir-darwin-ca924.firebaseapp.com',
+  projectId: 'fir-darwin-ca924',
+  storageBucket: 'fir-darwin-ca924.appspot.com',
+  messagingSenderId: '829740791170',
+  appId: '1:829740791170:web:75f8ee524c898ca2b34b9b',
+};
+
+initializeApp(firebaseConfig);
